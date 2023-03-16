2a46a7d1f48cc2543b623aceddff8d205a723f66
ViktoriaMalysh <vikyskamalyshzp@gmail.com>
2023-03-16 19:59:10 +0200

    fixed bugs

--- Requests/Api.js
+++ Requests/Api.js
@@ -1,108 +1,142 @@
 const axios = require("axios");
+const { reviewsList } = require("../config");
 const X_RapidAPI_Key = process.env.X_RapidAPI_Key;
 const X_RapidAPI_URL = process.env.X_RapidAPI_URL;
 
 module.exports.getTours = (req, res) => {
-  const data = req.body.data;
+    const data = req.body.data;
 
-  const options = {
-    method: "POST",
-    url: `${X_RapidAPI_URL}list`,
-    headers: {
-      "content-type": "application/json",
-      "X-RapidAPI-Key": String(X_RapidAPI_Key),
-      "X-RapidAPI-Host": "hotels4.p.rapidapi.com",
-    },
-    data: data,
-  };
+    const options = {
+        method: "POST",
+        url: `${X_RapidAPI_URL}list`,
+        headers: {
+            "content-type": "application/json",
+            "X-RapidAPI-Key": String(X_RapidAPI_Key),
+            "X-RapidAPI-Host": "hotels4.p.rapidapi.com",
+        },
+        data: data,
+    };
 
-  axios
-    .request(options)
-    .then((response) => {
-      res
-        .status(200)
-        .json({ tours: response.data.data.propertySearch.properties });
-    })
-    .catch((error) => {
-      console.log(error);
-      res.status(500).json({ message: error });
-    });
+    axios
+        .request(options)
+        .then((response) => {
+            console.log(response.data.data.propertySearch.properties);
+            res.status(200).json({
+                tours: response.data.data.propertySearch.properties,
+            });
+        })
+        .catch((error) => {
+            console.log(error);
+            res.status(500).json({ message: error });
+        });
 };
 
-module.exports.getTour = (req, res) => {
-  const tour = req.body.tour;
+module.exports.getTour = async (req, res) => {
+    const data = req.body.data;
 
-  const optionsGetReviews = {
-    method: "POST",
-    url: `${X_RapidAPI_URL}detail`,
-    headers: {
-      "content-type": "application/json",
-      "X-RapidAPI-Key": String(X_RapidAPI_Key),
-      "X-RapidAPI-Host": "hotels4.p.rapidapi.com",
-    },
-    data: {
-      currency: tour.currency,
-      eapid: 1,
-      locale: tour.locale,
-      siteId: 300000001,
-      propertyId: tour.propertyId,
-    },
-  };
+    let images;
+    let reviews;
+    let result;
 
-  axios
-    .request(optionsGetReviews)
-    .then((response) => {
-      const images = response.data.data.propertyInfo.propertyGallery.images.map(
-        (item) => {
-          return {
-            url: item.image.url,
-            description: item.image.description,
-          };
-        }
-      );
-      console.log("[images]:", images);
+    const optionsGetImages = {
+        method: "POST",
+        url: `${X_RapidAPI_URL}detail`,
+        headers: {
+            "content-type": "application/json",
+            "X-RapidAPI-Key": String(X_RapidAPI_Key),
+            "X-RapidAPI-Host": "hotels4.p.rapidapi.com",
+        },
+        data: data,
+    };
 
-      const res = response.data.data.propertyInfo.summary;
-      const tourDetails = {
-        id: res.id,
-        name: res.name,
-        map: res.map,
-        rating: res.overview.propertyRating.rating,
-        city: res.location.address.city,
-        coordinates: [
-          res.location.coordinates.latitude,
-          res.location.coordinates.longitude,
-        ],
-        images: [],
-        image: res.staticImage.url,
-      };
-      res.status(200).json({ tourDetails: tourDetails });
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).json({ message: error });
-    });
+    const optionsGetReviews = {
+        method: "POST",
+        url: `https://hotels4.p.rapidapi.com/reviews/v3/list`,
+        headers: {
+            "content-type": "application/json",
+            "X-RapidAPI-Key": String(X_RapidAPI_Key),
+            "X-RapidAPI-Host": "hotels4.p.rapidapi.com",
+        },
+        data: {
+            currency: data.currency,
+            eapid: data.eapid,
+            locale: data.locale,
+            siteId: data.siteId,
+            propertyId: data.propertyId,
+            size: 10,
+            startingIndex: 0,
+        },
+    };
 
-  // const responseGetReviews = await axios(optionsGetReviews);
+    try {
+        result = await axios.request(optionsGetImages);
+        images = result.data.data.propertyInfo.propertyGallery.images.map(
+            (item) => {
+                return {
+                    url: item.image.url,
+                    description: item.image.description,
+                };
+            }
+        );
+        result = result.data.data.propertyInfo.summary;
 
-  // const ress = {
-  //   guestReviews: {
-  //     starRating: response.data.data.body.propertyDescription.starRating,
-  //     reviewsCount: response.data.data.body.guestReviews.brands.total,
-  //     reviews:
-  //       responseGetReviews.data.reviewData.guestReviewGroups.guestReviews
-  //         .reviews,
-  //   },
-  //   price: {
-  //     currentPrice:
-  //       response.data.data.body.propertyDescription.featuredPrice.currentPrice
-  //         .formatted,
-  //     oldPrice:
-  //       response.data.data.body.propertyDescription.featuredPrice.oldPrice,
-  //   }
-  //   roomTypes: response.data.data.body.propertyDescription.roomTypeNames,
+        // reviews = await axios.request(optionsGetReviews);
 
-  // };
+        // const totalCount =
+        //     reviews.data.data.propertyInfo.reviewInfo.summary.totalCount.raw;
+        // reviews = reviews.data.data.propertyInfo.reviewInfo.reviews.map(
+        //     (item) => {
+        //         return {
+        //             text: item.text,
+        //             submissionTimeLocalized: item.submissionTimeLocalized,
+        //         };
+        //     }
+        // );
+
+        const tourDetails = {
+            id: result.id,
+            name: result.name,
+            map: result.map,
+            rating: result.overview.propertyRating.rating,
+            city: result.location.address.city,
+            coordinates: [
+                result.location.coordinates.latitude,
+                result.location.coordinates.longitude,
+            ],
+            images: images,
+            imageMap: result.location.staticImage.url,
+            addressLine: result.location.address.addressLine,
+            reviews: reviewsList,
+            // type: response.data.data.propertyInfo.reviewInfo.reviews[0].brandType,
+            // totalCount: totalCount,
+            totalCount: Math.floor(Math.random() * (500 - 100)) + 100
+        };
+        return res.status(200).json({ tourDetails: tourDetails });
+    } catch (exeption) {
+        console.error("[exeption]", exeption);
+        return res.status(500).json({ message: exeption });
+    }
+
+    // const responseGetReviews = await axios(optionsGetReviews);
+
+    // const ress = {
+    //   guestReviews: {
+    //     starRating: response.data.data.body.propertyDescription.starRating,
+    //     reviewsCount: response.data.data.body.guestReviews.brands.total,
+    //     reviews:
+    //       responseGetReviews.data.reviewData.guestReviewGroups.guestReviews
+    //         .reviews,
+    //   },
+    //   price: {
+    //     currentPrice:
+    //       response.data.data.body.propertyDescription.featuredPrice.currentPrice
+    //         .formatted,
+    //     oldPrice:
+    //       response.data.data.body.propertyDescription.featuredPrice.oldPrice,
+    //   }
+    //   roomTypes: response.data.data.body.propertyDescription.roomTypeNames,
+
+    // };
 };
 
 // [not used]
@@ -135,17 +169,17 @@ module.exports.getTour = (req, res) => {
 // };
 
 module.exports.getReviewsById = async function (req, res) {
-  try {
-    const config = req.body;
+    try {
+        const config = req.body;
 
-    const result = await axios(config);
+        const result = await axios(config);
 
-    const reviews =
-      result.data.reviewData.guestReviewGroups.guestReviews[0].reviews;
+        const reviews =
+            result.data.reviewData.guestReviewGroups.guestReviews[0].reviews;
 
-    res.status(200).json({ reviews: reviews });
-  } catch (err) {
-    console.log("Error: " + err);
-    res.status(500).json({ message: "Server have some problem" });
-  }
+        res.status(200).json({ reviews: reviews });
+    } catch (err) {
+        console.log("Error: " + err);
+        res.status(500).json({ message: "Server have some problem" });
+    }
 };
---
+++ config.js
@@ -0,0 +1,112 @@
+const reviewsList = [
+    {
+        itineraryId: "9005181001415",
+        reviewDbDate: "Aug 26, 2022",
+        rating: "10.0",
+        reviewer: {
+            name: "Peder",
+        },
+        badge: "Exceptional",
+        description: "Great location and close to everything",
+    },
+    {
+        itineraryId: "9210392298966",
+        reviewDbDate: "Aug 12, 2015",
+        rating: "10.0",
+        reviewer: {
+            name: "Anna",
+        },
+        badge: "Exceptional",
+        description: "Staff are the biggest let down of this hotel. Most are miserable (2 fellas in kitchen smile, as do bellboys) Standard room tiny, you can only get out of 1 side of the bed, no where for cases except on dressing table. We got used to it but wouldn't go back. Unfortunately we had 12 nights here.",
+    },
+    {
+        itineraryId: "8109450920117",
+        reviewDbDate: "Oct 10, 2019",
+        rating: "8.0",
+        reviewer: {
+            name: "Vladislav",
+        },
+        badge: "Very Good",
+        description:
+            "It hotel was clean and conveniently-located near the beach",
+    },
+    {
+        itineraryId: "7488861214824",
+        reviewDbDate: "Feb 13, 2021",
+        rating: "8.0",
+        reviewer: {
+            name: "William",
+        },
+        badge: "Very Good",
+        description:
+            "The hotel is set back from the main road which makes it nice and quiet. Room was very clean and comfortable, a little smaller than others we have stayed in.",
+    },
+    {
+        itineraryId: "8138169800954",
+        reviewDbDate: "Feb 3, 2021",
+        rating: "10.0",
+        reviewer: {
+            name: "Thomas",
+        },
+        badge: "Exceptional",
+        description:
+            "Stayed in two different rooms both a deluxe and supreme. Supreme was not worth the extra money as the “balcony” is non existent and you can’t actually sit out there. Otherwise really good location close to the beach, comfy pillows, and the free breakfast was decent too",
+    },
+    {
+        itineraryId: "8138169800954",
+        reviewDbDate: "May 25, 2020",
+        rating: "10.0",
+        reviewer: {
+            name: "Vadim",
+        },
+        badge: "Exceptional",
+        description: "Eventful stay with very friendly and helpful staff.",
+    },
+    {
+        itineraryId: "8138169800954",
+        reviewDbDate: "May 15, 2020",
+        rating: "8.0",
+        reviewer: {
+            name: "Colin",
+        },
+        badge: "Very Good",
+        description:
+            "Air conditioning was locked on a low level, making it difficult to sleep in the heat.",
+    },
+    {
+        itineraryId: "8138169800954",
+        reviewDate: "Aug 20, 2018",
+        rating: "4.0",
+        reviewer: {
+            name: "Henry",
+        },
+        badge: "Fair",
+        description: "Didn't like the rooms and service",
+    },
+    {
+        itineraryId: "8138169800954",
+        reviewDate: "Jul 27, 2017",
+        rating: "8.0",
+        reviewer: {
+            name: "Afra",
+        },
+        badge: "Exceptional",
+        description:
+            "We enjoyed our stay at this new property but unfortunately there aren't any rooms with a view, even though it says there is. There is construction in the front as well as on the side of the property, which can be noisy and doesn't alow for naps in the afternoon. We booked a room with a balcony and mountain view but apparently if you have a balony then you can't have a mountain view, so be careful when booking. Our balcony room was next to the contruction sight and we couldn't opent he door because of all the dust and noise. We requested to move rooms and only after insisting did they change us. The front desk were nice and helpful. overall it's a good place for price and location. Id come back but only after the construction is over with",
+    },
+
+    {
+        itineraryId: "8138169800954",
+        reviewDate: "Jun 15, 2016",
+        rating: "10.0",
+        reviewer: {
+            name: "Julia",
+        },
+        badge: "Good",
+        description:
+            "Good value for money close to beach",
+    },
+    
+];
+
+module.exports = { reviewsList };
